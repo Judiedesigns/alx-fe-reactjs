2f412e447e4d2fcfdf23030e99772525f07d71cb
Florence <florencekey22@gmail.com>
2025-09-09 08:58:27 -0700

    added bg-color to navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 
 function Navbar() {
     return(
-        <nav style={{ display: 'flex', gap: '20px', justifyContent: 'center', padding: '24px'}}>
+        <nav style={{ display: 'flex', gap: '20px', justifyContent: 'center', padding: '24px', backgroundColor: 'white'}}>
             <Link to='/' style={ {color: '#333333', textDecoration: "none", fontweight:'medium'}}>Home</Link>
             <Link to='/about' style={ {olor: '#333333', textDecoration: "none", fontweight:'medium'}}>About</Link>
             <Link to='/services' style={ {olor: '#333333', textDecoration: "none", fontweight:'medium'}}>Services</Link>
