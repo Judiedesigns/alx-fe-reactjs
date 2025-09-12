34cebad093b7c6a48b05500654152e8ded64242a
Florence <florencekey22@gmail.com>
2025-09-12 09:08:34 -0700

    Add and delete features

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
 import Home from './components/Home.jsx'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import RecipeDetails from './components/RecipeDetails.jsx'
-import EditRecipeForm from './components/EditRecipeForm.jsx'
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path="/recipe/:id" element={<RecipeDetails />} />
-      {/* <Route path='/recipe/edit/:id' element={<EditRecipeForm />}/> */}
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   )
 }
 
