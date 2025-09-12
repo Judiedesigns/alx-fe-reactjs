06c1d83c731cfdec32214ae18d21fe6186d66d2b
Florence <florencekey22@gmail.com>
2025-09-12 07:00:08 -0700

    recipe app with zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm'
+
+function App() {
+
+  return (
+    <>
+    <AddRecipeForm />
+     <RecipeList />
+    </>
+  )
+}
+
+export default App
