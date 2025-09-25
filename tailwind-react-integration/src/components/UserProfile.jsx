54aff528f83cb70e3db5a53976d2f6b2a556d98b
Florence <florencekey22@gmail.com>
2025-09-25 09:03:27 -0700

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,18 +2,12 @@ import user from '/user.jpg';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm my-20 mx-auto rounded-lg shadow-lg text-center">
-      <img src={user} alt="User" className='rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto object-cover'/>
-      <h1 className='text-lg sm:text-xl text-blue-800 my-4'>John Doe</h1>
-      <p className='text-gray-600 text-sm sm:text-base'> Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm my-20 mx-auto rounded-lg shadow-lg text-center">
+      <img src={user} alt="User" className='rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto object-cover'/>
+      <h1 className='sm:text-lg md:text-xl text-blue-800 my-4'>John Doe</h1>
+      <p className='text-gray-600 sm:text-sm md:text-base'> Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
 
 export default UserProfile;
-
-
-// Paragraph (p):
-
-// Set the text color to gray-600 (text-gray-600).
-// Make the font size slightly smaller than the heading (text-base).
