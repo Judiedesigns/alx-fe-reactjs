72c9c102cca7c941b268df1b40d265df5c8958cc
Florence <florencekey22@gmail.com>
2025-09-25 09:01:13 -0700

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,9 +2,8 @@ import user from '/user.jpg';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm my-20 mx-auto rounded-lg shadow-lg text-center">
-      <img src={user} alt="User" className='rounded-full w-24 h-24 sm:w-36 sm:
-      h-36 mx-auto object-cover'/>
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm my-20 mx-auto rounded-lg shadow-lg text-center">
+      <img src={user} alt="User" className='rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto object-cover'/>
       <h1 className='text-lg sm:text-xl text-blue-800 my-4'>John Doe</h1>
       <p className='text-gray-600 text-sm sm:text-base'> Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
