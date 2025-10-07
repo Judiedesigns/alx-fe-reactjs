58a4c75f8b20a93829749995af05a840fb977409
Florence <florencekey22@gmail.com>
2025-10-07 17:07:59 +0100

    implementation of recipe card navigation

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,7 @@
 import { useState,useEffect, use } from "react";
 import data from "../data.json";
+import { Link } from "react-router-dom";
+
 const Homepage = () => {
   const [recipes, setRecipes] = useState([]);
 
@@ -15,10 +17,12 @@ const Homepage = () => {
       <h1 className="text-2xl font-bold">Recipe Sharing Platform</h1>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
         {data.map((recipe) => (
-          <div key={recipe.id} className="border p-4 mb-4 rounded-lg hover:shadow-lg transition-shadow cursor-pointer" onClick={() => window.location.href = `/recipe/${recipe.id}`}>
-            <h2 className="text-xl font-bold">{recipe.title}</h2>
-            <p className="text-gray-600">{recipe.summary}</p>
-            <img className="w-[400px] mx-auto" src={recipe.image} alt={recipe.title} />
+          <div key={recipe.id} className="border p-4 mb-4 rounded-lg hover:shadow-lg transition-shadow">
+            <Link to={`/recipe/${recipe.id}`}>
+              <h2 className="text-xl font-bold">{recipe.title}</h2>
+              <p className="text-gray-600">{recipe.summary}</p>
+              <img className="w-[400px] mx-auto" src={recipe.image} alt={recipe.title} />
+            </Link>
           </div>
         ))} 
       </div>
